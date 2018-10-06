fc50695abb91e284b57234d4a4771b0a4a9b9a8d
TED Vortex <ted.vortex@gmail.com>
2018-10-06 23:06:58 +0300

    build(App): ESLint configuration and fix script

--- next.config.js
+++ next.config.js
@@ -1,17 +1,28 @@
 // next.config.js
-const fs = require('fs')
-const { join } = require('path')
-const { promisify } = require('util')
-const copyFile = promisify(fs.copyFile)
-const deploy = './deploy/'
+const fs = require('fs');
+const { join } = require('path');
+const { promisify } = require('util');
+
+const copyFile = promisify(fs.copyFile);
+const deploy = './deploy/';
+
+require('json5/lib/register');
 
 module.exports = {
-    exportPathMap: async function (defaultPathMap, {dev, dir, outDir, distDir, buildId}) {
-        if (dev) {
-            return defaultPathMap
-        }
+  webpack(config) {
+    config.node = { fs: 'empty' };
 
-        await copyFile(join(dir, `${deploy}CNAME`), join(outDir, 'CNAME'))
-        return defaultPathMap
+    return config;
+  },
+  webpackDevMiddleware: config => config,
+  async exportPathMap(defaultPathMap, {
+    dev, dir, outDir,
+  }) {
+    if (dev) {
+      return defaultPathMap;
     }
-}
+
+    await copyFile(join(dir, `${deploy}CNAME`), join(outDir, 'CNAME'));
+    return defaultPathMap;
+  },
+};
