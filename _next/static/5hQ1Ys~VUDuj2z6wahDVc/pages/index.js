4069c19bc78192808a46fa80bb5805c6647f46fb
Travis CI (from Travis CI) <deploy@travis-ci.org>
2018-11-29 02:43:24 +0000

    Deploy vortex.name to github.com/0-vortex/vortex.name.git:gh-pages

--- _next/static/5hQ1Ys~VUDuj2z6wahDVc/pages/index.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{206:function(n,e,t){__NEXT_REGISTER_PAGE("/",function(){return n.exports=t(207),{page:n.exports.default}})},207:function(n,e,t){"use strict";t.r(e);var o=t(8),u=t.n(o);e.default=function(){return u.a.createElement("div",null,"Welcome to next.js!")}}},[[206,1,0]]]);
